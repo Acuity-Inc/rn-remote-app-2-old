3e9c2b2099655ad7c02de0870422b316e3f8424f
ArmaniGiles <bless727@gmail.com>
2023-11-29 17:37:09 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,37 +1,36 @@
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   View,
- } from 'react-native';
- 
- function App(): JSX.Element {
-   return (
-     <SafeAreaView>
-       <StatusBar />
-       <ScrollView contentContainerStyle={styles.scrollContainer}>
-         <View style={styles.mainContainer}>
-           <Text>Mini App Two Test</Text>
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- }
- 
- const styles = StyleSheet.create({
-   mainContainer: {
-     flex: 1,
-     justifyContent: 'center',
-     alignItems: 'center',
-     backgroundColor: '#ACDDDE',
-   },
-   scrollContainer: {
-     height: '100%',
-   },
- });
- 
- export default App;
- 
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.mainContainer}>
+          <Text>Mini App Two Test</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ACDDDE',
+  },
+  scrollContainer: {
+    height: '100%',
+  },
+});
+
+export default App;
